0542e9b068e5e1f26458fdfc55793ed0bfad2982
= <nncl@live.com>
2019-12-24 22:20:52 -0300

    Adding refresh token

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { StatusBar } from 'react-native';
 import App from './App';
 
 import './config/ReactotronConfig';
-
+import api from '~/services/api';
 import { persistor, store } from './store';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Index() {
   return (
@@ -18,3 +19,12 @@ export default function Index() {
     </Provider>
   );
 }
+
+const { dispatch } = store;
+api.interceptors.response.use(null, err => {
+  const { status } = err.response;
+  if (status === 401) {
+    dispatch(signOut());
+  }
+  return err;
+});
--- src/services/api.js
+++ src/services/api.js
@@ -4,12 +4,4 @@ const api = axios.create({
   baseURL: `http://localhost:3333`, // fixme
 });
 
-api.interceptors.response.use(null, err => {
-  const { status } = err.response;
-  if (status === 401) {
-    // FIXME
-  }
-  return err;
-});
-
 export default api;

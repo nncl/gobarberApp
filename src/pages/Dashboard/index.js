3a95fe5e77608c7653e8966380e09a578d2355f8
= <nncl@live.com>
2019-12-25 23:59:41 -0300

    Fixing route changing blur

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,33 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import { withNavigationFocus } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
 import { Container, Title, List } from './styles';
 import Appointment from '~/components/Appointment';
 import api from '~/services/api';
 
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
   const [appointments, setAppointment] = useState([]);
-  useEffect(() => {
-    async function getAppointments() {
-      const response = await api.get(`appointments`);
-      setAppointment(response.data);
-    }
 
-    getAppointments();
-  }, []);
+  async function getAppointments() {
+    const response = await api.get(`appointments`);
+    setAppointment(response.data);
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      getAppointments();
+    }
+  }, [isFocused]);
 
   async function handleCancel(id) {
     const response = await api.delete(`appointments/${id}`);
 
     setAppointment(
-      appointments.map(appointment =>
-        appointment.id === id
-          ? {
-              ...appointment,
-              canceled_at: response.data.canceled_at,
-            }
-          : appointment
-      )
+      appointments
+        .map(appointment =>
+          appointment.id === id
+            ? {
+                ...appointment,
+                canceled_at: response.data.canceled_at,
+              }
+            : appointment
+        )
+        .filter(item => !item.canceled_at)
     );
   }
 
@@ -54,3 +60,5 @@ Dashboard.navigationOptions = {
     <Icon name="event" size={20} color={tintColor} />
   ),
 };
+
+export default withNavigationFocus(Dashboard);
